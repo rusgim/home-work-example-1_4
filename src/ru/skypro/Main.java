bdf278c16589c9958824c796f3e7c3a3690c2fdd
Ruslan <gimazevs@mail.ru>
2022-02-06 11:13:57 +0300

    первое задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,17 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int p = 0;
+        while (p < 10) {
+            p++;
+            System.out.print(" " + p);
+        }
+        System.out.print("\n");
+        int re = 1;
+        int st = 11;
+        for (int rest = 0; rest < 10; rest++) {
+            st = st - re;
+            System.out.print(" " + st);
+        }
     }
 }
